d85b48d6aa93515ecb5c642a1c898dd5b0ad4bc3
patia141 <patia141@gmail.com>
2022-11-03 12:17:43 +0100

    l

--- Lab3/6.py
+++ Lab3/6.py
@@ -3,18 +3,31 @@ przedziału 0 – 100 pomijać dalsze wykonywanie pętli. Sprawdź działanie pr
 nieskończoną, czyli taką której warunek zawsze jest prawdziwy. Zakończ działanie pętli przy użyciu instrukcji
 break.'''
 
-n = int(input("Podaj liczbę studentów: "))
+'''n = int(input("Podaj liczbę studentów: "))
 x=1
-b=0
+b=0 #suma pkt
 
 while x<=n:
     a = int(input(f"podaj liczbę punktów {x} studenta: "))
-    if 0 <= a <= 100:
-        b += a
-        x += 1
-    else:
-        x+=n
+    if a<0 or a>100:
         continue
+    b += a
+    x += 1
 
 sr=b/n
-print("średnia wynosi: ", sr)
+print("średnia wynosi: ", sr)'''
+
+n = int(input("Proszę podać liczbę studentów: "))
+a = 1 #wyświetlanie od 1 studenta
+c = 0 #podstawienie pod dodawanie punktów
+while True:
+    b = int(input(f"Proszę podać punkty studenta {a}: "))
+    if b<0 or b>100:
+        continue
+    c += b #sumowanie punktów studentów
+    a += 1 #przechodzenie do kolejnego studenta
+    if a==n+1:
+        break
+
+y = c/n #obliczanie średniej
+print("Średnia wszystkich studentów",y)
---
+++ wejsciowka/1.py
@@ -0,0 +1,8 @@
+#gr 1
+a = int(input("podaj 1 liczbe: "))
+b = int(input("Podaj 2 liczbe: "))
+
+if a>b:
+    print (b)
+else:
+    print (a)
