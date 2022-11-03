9b232f706592ad6ed90d5a1ad0ea9af1025ea245
patia141 <patia141@gmail.com>
2022-11-03 13:27:03 +0100

    hhh

---
+++ Lab4/3.py
@@ -0,0 +1,12 @@
+'''Utwórz pustą listę zwierzeta. Następnie
+• Dodaj kilka nazw zwierząt do listy
+• Posortuj listę
+• Wyświetl pierwszy oraz trzy ostatnie elementy na liście
+• Wyświetl informację o liczbie zwierząt na liście'''
+
+zwierzeta=[]
+zwierzeta = ["kot","pies","ryba","kaczka","tygrys","slon"]
+zwierzeta.sort()
+print(zwierzeta[0])
+print(zwierzeta[-3:])
+print(len(zwierzeta))
